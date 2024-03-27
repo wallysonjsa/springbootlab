17ac737201ed069de7ccdd1a531a40b3cffb62cb
Araujo <wallyson.j.araujo@accenture.com>
2024-03-27 18:21:11 -0300

    Test 7 fail - expected 404 not found but was 200 OK

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -31,4 +31,12 @@ class CashCardApplicationTests {
 		Double amount = documentContext.read("$.amount");
 		assertThat(amount).isEqualTo(123.45);
 	}
+	
+	@Test
+	void shouldNotReturnACashCardWithAnUnknowmId(){
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
+	}	
 }
