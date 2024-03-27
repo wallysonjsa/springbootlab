21b28ed65c8e612ba150aa729bac9ea5cf342210
Araujo <wallyson.j.araujo@accenture.com>
2024-03-27 15:52:14 -0300

    Test 5 fail - Expected to find an object with property ['id'] in path $ but found 'java.lang.String'

---
+++ src/main/java/example/cashcard/CashCardController.java
@@ -0,0 +1,17 @@
+package example.cashcard;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcards")
+class CashCardController {
+
+    @GetMapping("/{requestedId}")
+    private ResponseEntity<String> findById() {
+        return ResponseEntity.ok("{}");
+    }
+}
