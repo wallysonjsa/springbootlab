c3247ef735e684be2028b153599a954697542ada
Araujo <wallyson.j.araujo@accenture.com>
2024-03-27 15:03:52 -0300

    Test 2 fail - cannot find symbol

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -1,12 +1,28 @@
 package example.cashcard;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JsonbTester;
+
+import java.io.IOException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@JsonTest
 public class CashCardJsonTest {
 
 	@Test
 	void myFirstTest() {
 		assertThat(42).isEqualTo(42);
+	}
+	
+	@Autowired
+	private JacksonTester<CashCard> json;
+	
+	@Test
+	void cashCardSerializationTest() throws IOException {
+		CashCard cashCard = new CashCard(99L, 123.45);
+		assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
 	}
 }
