a6ecd3c242d496381692a67864d1c32d6ffd93c5
Araujo <wallyson.j.araujo@accenture.com>
2024-03-27 14:56:04 -0300

    Test fail

---
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,12 @@
+package example.cashcard;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CashCardJsonTest {
+
+	@Test
+	void myFirstTest() {
+		assertThat(1).isEqualTo(42);
+	}
+}
