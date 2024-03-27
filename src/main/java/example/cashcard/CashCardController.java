c170ebc897f5b061c6e83c17e98ec62d9393d6b3
Araujo <wallyson.j.araujo@accenture.com>
2024-03-27 18:22:28 -0300

    Test 7 pass

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -11,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 class CashCardController {
 
     @GetMapping("/{requestedId}")
-    private ResponseEntity<CashCard> findById() {
+   private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+    if(requestedId.equals(99L)){
         CashCard cashCard = new CashCard(99L, 123.45);
         return ResponseEntity.ok(cashCard);
+    }else{
+        return ResponseEntity.notFound().build();
     }
+   }
 }
