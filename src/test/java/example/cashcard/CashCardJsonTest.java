b4baab5eaba31b89671b415f89ca0818c97412fa
Araujo <wallyson.j.araujo@accenture.com>
2024-03-27 15:12:31 -0300

    Test 3 pass. Test 4 fail: Unable to load JSON from class path resource [example/cashcard/expected.json]

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -3,6 +3,7 @@ package example.cashcard;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.json.JsonbTester;
 
 import java.io.IOException;
@@ -24,5 +25,10 @@ public class CashCardJsonTest {
 	void cashCardSerializationTest() throws IOException {
 		CashCard cashCard = new CashCard(99L, 123.45);
 		assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
+		assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
+		assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
+		assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
+		assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
+		
 	}
 }
