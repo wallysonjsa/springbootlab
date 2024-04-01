755c91acd2c618616bfcb7b44702efe99fbdfae1
Araujo <wallyson.j.araujo@accenture.com>
2024-04-01 15:22:16 -0300

    Test fail - Table CASH_CARD not found (this database is empty); SQL statement

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cashcards")
@@ -17,11 +18,12 @@ class CashCardController {
 	
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-	    if(requestedId.equals(99L)){
-	        CashCard cashCard = new CashCard(99L, 123.45);
-	        return ResponseEntity.ok(cashCard);
-	    }else{
-	        return ResponseEntity.notFound().build();
-	    }
+    	Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+    	
+    	if(cashCardOptional.isPresent()) {
+    		return ResponseEntity.ok(cashCardOptional.get());
+    	}else{
+    		return ResponseEntity.notFound().build();
+    	}
    }
 }
